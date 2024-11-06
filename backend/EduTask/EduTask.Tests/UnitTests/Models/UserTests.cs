f30b29749ab147e4bd3c13ab032fdeb4b8007c05
Jenny03b <bujallajennifer@gmail.com>
2024-11-06 12:40:04 +0100

    Init test project

---
+++ backend/EduTask/EduTask.Tests/UnitTests/Models/UserTests.cs
@@ -0,0 +1,18 @@
+﻿using EduTask.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduTask.Tests.UnitTests.Models
+{
+    public class UserTests
+    {
+        [Fact]
+        public void Shoud_Create_User()
+        {
+            var user = new User();
+        }
+    }
+}
